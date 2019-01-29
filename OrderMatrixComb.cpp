4dadb4658f8580f90cb56a034be8f0defb856a12
SavaSuper <46940256+SavaSuper@users.noreply.github.com>
2019-01-29 18:11:06 +0200

    Create OrderMatrixComb.cpp

---
+++ OrderMatrixComb.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	cout << "\n\tПоиск номера строки с наибольшей суммой \n";
+	cout<<("\n\tОбъеденяем две таблицы в одну A+B=C\n");
+
+	int n = 10;
+	int A[10];
+	int B[10];
+	int C[20];
+
+
+	int k = 0;
+	for (int i = 0; i <n; i++) {
+		A[i] = (int)(rand()%10);
+		B[i] = (int)(rand() % 10);
+		C[k] = A[i]; C[k + 1] = B[i];
+		k += 2;
+	}
+
+
+	
+	
+
+	for (int i = 0; i <n; i++) {
+		printf("A[%d]=%d  ", i, A[i]);
+	}
+	cout<<("\n");
+
+	for (int i = 0; i <n; i++) {
+		printf("B[%d]=%d  ", i, B[i]);
+	}
+	cout<<("\n");
+
+
+	for (int i = 0; i <2 * n; i++) {
+		printf("C[%d]=%d\n", i, C[i]);
+	}
+	cout<<("\n");
+
+	cout << endl << endl << "\t";
+	system("pause");
+	return 0;
+}
